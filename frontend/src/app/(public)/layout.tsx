0187ebcf2823df4b2450ac29bd2dd197d8122895
Eduardo Zago <54417621+eduardozago@users.noreply.github.com>
2025-04-16 11:14:16 -0300

    feat: frontend auth (#10)
    * feat: create login and signup pages
    
    * refactor: update middleware to protect all routes except signup and login
    
    * feat: add auth validation to restrict access on private routes
    
    * feat: add public access for signed out users and dashboard redirection for signed in users

--- frontend/src/app/(public)/layout.tsx
+++ frontend/src/app/(public)/layout.tsx
@@ -1,9 +1,26 @@
+'use client'
+
+import { SignedOut, useAuth } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function PublicLayout({ children }: Readonly<{
     children: React.ReactNode;
   }>) {
+    const { isSignedIn } = useAuth()
+    const router = useRouter()
+
+    useEffect(() => {
+        if (isSignedIn) {
+            router.replace('/dashboard')
+        }
+    }, [isSignedIn])
+
     return (
-        <div className="flex items-center justify-center h-[100vh]">
-            {children}
-        </div>
+        <SignedOut>
+            <div className="flex items-center justify-center h-[100vh]">
+                {children}
+            </div>
+        </SignedOut>
     )
 }
