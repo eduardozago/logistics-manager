d03302f0f0978b1f05618f208f5967d0ab1d23db
Eduardo Zago <zagoe22@gmail.com>
2025-04-16 16:44:20 -0300

    feat: add header component with logo and sign-out functionality

--- frontend/src/app/(private)/layout.tsx
+++ frontend/src/app/(private)/layout.tsx
@@ -1,10 +1,12 @@
 import { SignedIn } from "@clerk/nextjs";
+import Header from "./components/header";
 
 export default function PrivateLayout({ children }: Readonly<{
     children: React.ReactNode;
   }>) {
     return (
         <SignedIn>
+            <Header />
             {children}
         </SignedIn>
     )
