4f2ea5f27d8212dc322f9abe7696360e0ed0ab99
Eduardo Zago <zagoe22@gmail.com>
2025-04-16 09:32:29 -0300

    chore: set up authentication with clerk

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter()
 
@@ -19,7 +20,9 @@ export default function RootLayout({
       <body
         className={`${inter.className} antialiased`}
       >
-        {children}
+        <ClerkProvider>
+          {children}
+        </ClerkProvider>
       </body>
     </html>
   );
