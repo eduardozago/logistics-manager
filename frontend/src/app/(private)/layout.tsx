f6446f49b495b10f3474ea187244667ae4bba85e
Eduardo Zago <zagoe22@gmail.com>
2025-04-23 17:06:41 -0300

    refactor: update private layout styles for responsiveness

--- frontend/src/app/(private)/layout.tsx
+++ frontend/src/app/(private)/layout.tsx
@@ -10,7 +10,7 @@ export default function PrivateLayout({ children }: Readonly<{
             <Header />
             <div className="flex">
                 <Sidebar />
-                <main className="ml-[14rem] p-[3rem] w-full">
+                <main className="px-[1rem] md:px-[2rem] md:pt-[2rem] pt-[4rem] w-full">
                     {children}
                 </main>
             </div>
