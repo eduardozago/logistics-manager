8e0a255fd3df1739e996be827fe438ddf193eb90
Eduardo Zago <zagoe22@gmail.com>
2025-04-25 09:32:28 -0300

    feat: implement ClerkAuthGuard and public decorator for route protection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { ClerkClientProvider } from './providers/clerk-client.provider'
+import { AuthModule } from './auth/auth.module'
+import { APP_GUARD } from '@nestjs/core'
+import { ClerkAuthGuard } from './auth/clerk.guard'
 import { AppController } from './app.controller'
 
 @Module({
@@ -8,8 +11,15 @@ import { AppController } from './app.controller'
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AuthModule,
   ],
   controllers: [AppController],
-  providers: [ClerkClientProvider],
+  providers: [
+    ClerkClientProvider,
+    {
+      provide: APP_GUARD,
+      useClass: ClerkAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
